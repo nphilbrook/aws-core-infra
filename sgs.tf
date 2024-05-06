e299cb0f104bde887ebd5da3bd0f13d2c933dd56
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-05-06 16:14:43 -0400

    https

--- sgs.tf
+++ sgs.tf
@@ -13,6 +13,15 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
   to_port     = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.allow_all.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
 resource "aws_vpc_security_group_ingress_rule" "http" {
   security_group_id = aws_security_group.allow_all.id
 
