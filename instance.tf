48dfb21761339a84876039786122349baee46241
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-05-08 05:22:44 -0400

    just the record, please

--- instance.tf
+++ instance.tf
@@ -53,110 +53,118 @@ resource "aws_route53_zone" "primary" {
   name = "dev.exceptbuses.com"
 }
 
-module "acm" {
-  source  = "terraform-aws-modules/acm/aws"
-  version = "~>5.0.0"
-
-  domain_name       = "tfe.${local.subdomain}"
-  zone_id           = aws_route53_zone.primary.id
-  validation_method = "DNS"
+resource "aws_route53_record" "tfe" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "tfe.${local.subdomain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.tfe.public_ip]
 }
 
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~>9.9.0"
+# module "acm" {
+#   source  = "terraform-aws-modules/acm/aws"
+#   version = "~>5.0.0"
 
-  name    = local.subdomain
-  enable_deletion_protection = false
+#   domain_name       = "tfe.${local.subdomain}"
+#   zone_id           = aws_route53_zone.primary.id
+#   validation_method = "DNS"
+# }
 
-  # Security Group
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "~>9.9.0"
 
-  listeners = {
-    fixed-response = {
-      port     = 80
-      protocol = "HTTP"
-      fixed_response = {
-        content_type = "text/plain"
-        message_body = "Hella World\n"
-        status_code  = "200"
-      }
-    }
+#   name    = local.subdomain
+#   enable_deletion_protection = false
 
-    tfe-https = {
-      port                        = 443
-      protocol                    = "HTTPS"
-      ssl_policy                  = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
-      certificate_arn             = module.acm.acm_certificate_arn
+#   # Security Group
+#   security_group_ingress_rules = {
+#     all_http = {
+#       from_port   = 80
+#       to_port     = 80
+#       ip_protocol = "tcp"
+#       description = "HTTP web traffic"
+#       cidr_ipv4   = "0.0.0.0/0"
+#     }
+#     all_https = {
+#       from_port   = 443
+#       to_port     = 443
+#       ip_protocol = "tcp"
+#       description = "HTTPS web traffic"
+#       cidr_ipv4   = "0.0.0.0/0"
+#     }
+#   }
+#   security_group_egress_rules = {
+#     all = {
+#       ip_protocol = "-1"
+#       cidr_ipv4   = "0.0.0.0/0"
+#     }
+#   }
 
-      forward = {
-        target_group_key = "tfe"
-      }
+#   listeners = {
+#     fixed-response = {
+#       port     = 80
+#       protocol = "HTTP"
+#       fixed_response = {
+#         content_type = "text/plain"
+#         message_body = "Hella World\n"
+#         status_code  = "200"
+#       }
+#     }
 
-      rules = {
-        fixed-response = {
-          actions = [{
-            type         = "fixed-response"
-            content_type = "text/plain"
-            status_code  = 200
-            message_body = "Hella World but secure\n"
-          }]
+#     tfe-https = {
+#       port                        = 443
+#       protocol                    = "HTTPS"
+#       ssl_policy                  = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
+#       certificate_arn             = module.acm.acm_certificate_arn
 
-          conditions = [{
-            http_header = {
-              http_header_name = "x-Gimme-Fixed-Response"
-              values           = ["yes", "please", "right now"]
-            }
-          }]
-        }
-      }
-    }
-  }
-  target_groups = {
-    "tfe" = {
-      target_type       = "ip"
-      target_id         = aws_instance.tfe.private_ip
-      name_prefix       = "tfe-"
-      protocol          = "HTTP"
-      port              = 443
-      create_attachment = false
-      # health_check = {
-      #   enabled  = true
-      #   interval = 30
-      #   path     = "/health"
-      #   protocol = "HTTP"
-      #   matcher  = "200"
-      # }
-    }
-  }
+#       forward = {
+#         target_group_key = "tfe"
+#       }
 
-  # Route53 Record(s)
-  route53_records = {
-    A = {
-      name    = "tfe.${local.subdomain}"
-      type    = "A"
-      zone_id = aws_route53_zone.primary.id
-    }
-  }
-}
+#       rules = {
+#         fixed-response = {
+#           actions = [{
+#             type         = "fixed-response"
+#             content_type = "text/plain"
+#             status_code  = 200
+#             message_body = "Hella World but secure\n"
+#           }]
+
+#           conditions = [{
+#             http_header = {
+#               http_header_name = "x-Gimme-Fixed-Response"
+#               values           = ["yes", "please", "right now"]
+#             }
+#           }]
+#         }
+#       }
+#     }
+#   }
+#   target_groups = {
+#     "tfe" = {
+#       target_type       = "ip"
+#       target_id         = aws_instance.tfe.private_ip
+#       name_prefix       = "tfe-"
+#       protocol          = "HTTP"
+#       port              = 443
+#       create_attachment = false
+#       # health_check = {
+#       #   enabled  = true
+#       #   interval = 30
+#       #   path     = "/health"
+#       #   protocol = "HTTP"
+#       #   matcher  = "200"
+#       # }
+#     }
+#   }
+
+#   # Route53 Record(s)
+#   route53_records = {
+#     A = {
+#       name    = "tfe.${local.subdomain}"
+#       type    = "A"
+#       zone_id = aws_route53_zone.primary.id
+#     }
+#   }
+# }
