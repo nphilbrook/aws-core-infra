36e3e8ee476b7b341792709f2ca459332a0f4b75
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-07 15:27:00 -0400

    transit attachments?

--- transit_gateway.tf
+++ transit_gateway.tf
@@ -3,6 +3,7 @@ locals {
 }
 
 resource "aws_ec2_transit_gateway" "example" {
+  # implied e2 provider
   tags = {
     Name = "example-tgw"
   }
@@ -27,3 +28,35 @@ resource "aws_ram_resource_association" "example" {
 # resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "example" {
 #   transit_gateway_attachment_id = "TBD" # hcp_aws_transit_gateway_attachment.example.provider_transit_gateway_attachment_id
 # }
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "e2" {
+  subnet_ids         = [aws_subnet.e2.id]
+  transit_gateway_id = aws_ec2_transit_gateway.example.id
+  vpc_id             = aws_vpc.e2.id
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "e1" {
+  subnet_ids         = [aws_subnet.e1.id]
+  transit_gateway_id = aws_ec2_transit_gateway.example.id
+  vpc_id             = aws_vpc.e1.id
+}
+
+# Since I used the default here...
+data "aws_vpc" "default_w2" {
+  provider = aws.usw2
+  default  = true
+}
+
+data "aws_subnets" "w2" {
+  provider = aws.usw2
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default_w2.id]
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "w2" {
+  subnet_ids         = [data.aws_subnets.w2.ids[0]]
+  transit_gateway_id = aws_ec2_transit_gateway.example.id
+  vpc_id             = data.aws_vpc.default_w2.id
+}
