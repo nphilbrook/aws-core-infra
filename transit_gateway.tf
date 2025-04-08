d7322fb1e26c2f089609e7383ad7e034cd95a2d4
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-08 15:36:21 -0400

    add the RAM stuff

--- transit_gateway.tf
+++ transit_gateway.tf
@@ -11,25 +11,6 @@ resource "aws_ec2_transit_gateway" "e2" {
   }
 }
 
-# resource "aws_ram_resource_share" "example" {
-#   name                      = "example-resource-share"
-#   allow_external_principals = true
-# }
-
-# resource "aws_ram_principal_association" "example" {
-#   resource_share_arn = aws_ram_resource_share.example.arn
-#   principal          = local.hvn_account_id
-# }
-
-# resource "aws_ram_resource_association" "example" {
-#   resource_share_arn = aws_ram_resource_share.example.arn
-#   resource_arn       = aws_ec2_transit_gateway.e2.arn
-# }
-
-# resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "example" {
-#   transit_gateway_attachment_id = "TBD" # hcp_aws_transit_gateway_attachment.example.provider_transit_gateway_attachment_id
-# }
-
 resource "aws_ec2_transit_gateway_vpc_attachment" "e2" {
   # implied e2 provider
   subnet_ids         = [aws_subnet.e2.id]
@@ -44,6 +25,29 @@ resource "aws_ec2_transit_gateway" "e1" {
     Name = "tgw-e1"
   }
 }
+
+resource "aws_ram_resource_share" "e1_share" {
+  provider                  = aws.use1
+  name                      = "e1-resource-share"
+  allow_external_principals = true
+}
+
+resource "aws_ram_principal_association" "e1_pa" {
+  provider           = aws.use1
+  resource_share_arn = aws_ram_resource_share.e1_share.arn
+  principal          = local.hvn_account_id
+}
+
+resource "aws_ram_resource_association" "e1_rra" {
+  provider           = aws.use1
+  resource_share_arn = aws_ram_resource_share.e1_share.arn
+  resource_arn       = aws_ec2_transit_gateway.e1.arn
+}
+
+# resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "example" {
+#   provider                       = aws.use1
+#   transit_gateway_attachment_id = "TBD" # hcp_aws_transit_gateway_attachment.example.provider_transit_gateway_attachment_id
+# }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "e1" {
   provider           = aws.use1
