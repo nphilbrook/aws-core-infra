08974c7589064afcf4304f013f1262f30fc7e6ee
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-11 14:07:07 -0400

    refactor loacl

--- r53.tf
+++ r53.tf
@@ -5,6 +5,5 @@ locals {
 # NOTE - delegation must be set up manually on dns.he.net
 # since this apex domain is not managed in Route53
 resource "aws_route53_zone" "primary" {
-  name = "dev.exceptbuses.com"
+  name = local.subdomain
 }
-
