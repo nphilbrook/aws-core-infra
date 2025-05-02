a17841d3931dbd8b7674d044ae4b9cf1b18e6599
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-02 12:46:39 -0400

    instance profile

--- agent.tf
+++ agent.tf
@@ -1,7 +1,50 @@
+data "aws_iam_policy_document" "agent_assume_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "agent_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "agent_policy" {
+  name   = "agent_policy"
+  policy = data.aws_iam_policy_document.agent_policy.json
+}
+
+resource "aws_iam_role" "agent_role" {
+  name               = "agent_role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.agent_assume_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "agent_policy_attachment" {
+  role       = aws_iam_role.agent_role.name
+  policy_arn = aws_iam_policy.agent_policy.arn
+}
+
+resource "aws_iam_instance_profile" "agent_profile" {
+  name = "agent_profile"
+  role = aws_iam_role.agent_role.name
+}
+
 resource "aws_instance" "agent" {
   provider                    = aws.usw2
   ami                         = data.aws_ami.rhel9_w2.id
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.agent_profile.name
   instance_type               = local.jump_instance_type
   key_name                    = aws_key_pair.acme_w2.key_name
   user_data                   = file("${path.module}/user_data.sh")
