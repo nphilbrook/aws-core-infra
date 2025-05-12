c4cccc0015427138dd8c2f34f11619d4c0597dec
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-12 10:36:46 -0400

    moar user data logging

--- user_data.sh
+++ user_data.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+STAMP=$(date -u)
+echo "Running at $STAMP" >> /root/ugh-wtf.log
+echo "Running at $STAMP"
+
 dnf install -y git awscli docker
 export TFC_AGENT_TOKEN=$(aws secretsmanager get-secret-value --secret-id tfc-agent-token|jq -r .SecretString)
 export TFC_AGENT_NAME=$(hostname)-1
@@ -9,3 +13,6 @@ export TFC_AGENT_AUTO_UPDATE=disabled
 docker run -d -e TFC_AGENT_TOKEN -e TFC_AGENT_NAME docker.io/hashicorp/tfc-agent:$VERSION
 export TFC_AGENT_NAME=$(hostname)-2
 docker run -d -e TFC_AGENT_TOKEN -e TFC_AGENT_NAME docker.io/hashicorp/tfc-agent:$VERSION
+
+echo "Finished Running at $STAMP" >> /root/ugh-wtf.log
+echo "Finished Running at $STAMP"
