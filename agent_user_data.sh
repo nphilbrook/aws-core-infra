14ca070e40e7a2d1c08c11e8ea104eace07328e2
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-14 09:23:30 -0400

    supervised agent

---
+++ agent_user_data.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+dnf install -y git awscli docker
+
+cat <<'EOF' > /usr/local/bin/tfc-agent-wrapper
+#!/bin/bash
+
+export TFC_AGENT_TOKEN=$(aws secretsmanager get-secret-value --secret-id tfc-agent-token|jq -r .SecretString)
+export TFC_AGENT_NAME=$(hostname)-$1
+export TFC_AGENT_AUTO_UPDATE=disabled
+# Current as of 2025-05-14
+export VERSION=1.22.2
+docker run -e TFC_AGENT_TOKEN -e TFC_AGENT_NAME -e TFC_AGENT_AUTO_UPDATE docker.io/hashicorp/tfc-agent:$VERSION
+EOF
+
+chmod 775 tfc-agent-wrapper
+
+cat <<EOF > /etc/systemd/system/tfc-agent@.service
+[Unit]
+Description=HCP Terraform Agent
+After=network.target
+
+[Service]
+User=root
+ExecStart=/usr/local/bin/tfc-agent-wrapper %I
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable tfc-agent@1.service
+sudo systemctl enable tfc-agent@2.service
+
+sudo systemctl start tfc-agent@1.service
+sudo systemctl start tfc-agent@2.service
--- user_data.sh
+++ user_data.sh
@@ -29,9 +29,9 @@ export TFC_AGENT_NAME=$(hostname)-1
 # Current as of 2025-05-02
 export VERSION=1.22.1
 export TFC_AGENT_AUTO_UPDATE=disabled
-docker run -d -e TFC_AGENT_TOKEN -e TFC_AGENT_NAME docker.io/hashicorp/tfc-agent:$VERSION
+docker run -d -e TFC_AGENT_TOKEN -e TFC_AGENT_NAME -e TFC_AGENT_AUTO_UPDATE docker.io/hashicorp/tfc-agent:$VERSION
 export TFC_AGENT_NAME=$(hostname)-2
-docker run -d -e TFC_AGENT_TOKEN -e TFC_AGENT_NAME docker.io/hashicorp/tfc-agent:$VERSION
+docker run -d -e TFC_AGENT_TOKEN -e TFC_AGENT_NAME -e TFC_AGENT_AUTO_UPDATE docker.io/hashicorp/tfc-agent:$VERSION
 
 echo "Finished Running at $STAMP" >> /root/ugh-wtf.log
 echo "Finished Running at $STAMP"
