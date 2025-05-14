23430fe67e0dbf36ab49c454745460e7ed6d7f83
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-14 13:01:30 -0400

    Sure, every time

--- agent_user_data.sh
+++ agent_user_data.sh
@@ -1,3 +1,22 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment;
+ filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
 #!/bin/bash
 
 dnf install -y git awscli docker
@@ -44,3 +63,4 @@ do
     sudo systemctl enable tfc-agent@$i.service
     sudo systemctl start tfc-agent@$i.service
 done
+--//--
