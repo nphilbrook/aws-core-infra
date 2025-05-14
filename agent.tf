428629f32ecb8b7cfc720f547d2213b4da641bf9
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-14 09:40:23 -0400

    remove old agent

--- agent.tf
+++ agent.tf
@@ -40,31 +40,31 @@ resource "aws_iam_instance_profile" "agent_profile" {
   role = aws_iam_role.agent_role.name
 }
 
-resource "aws_instance" "agent" {
-  provider                    = aws.usw2
-  ami                         = data.aws_ami.rhel9_w2.id
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.agent_profile.name
-  instance_type               = local.jump_instance_type
-  key_name                    = aws_key_pair.acme_w2.key_name
-  user_data                   = file("${path.module}/user_data.sh")
-  vpc_security_group_ids      = [aws_security_group.allow_ssh_w2.id]
-  tags = { Name = "agent",
-    owner = "nick.philbrook@hashicorp.com",
-    TTL   = 0
-  }
-  lifecycle {
-    ignore_changes = [ami]
-  }
-}
+# resource "aws_instance" "agent" {
+#   provider                    = aws.usw2
+#   ami                         = data.aws_ami.rhel9_w2.id
+#   associate_public_ip_address = true
+#   iam_instance_profile        = aws_iam_instance_profile.agent_profile.name
+#   instance_type               = local.jump_instance_type
+#   key_name                    = aws_key_pair.acme_w2.key_name
+#   user_data                   = file("${path.module}/user_data.sh")
+#   vpc_security_group_ids      = [aws_security_group.allow_ssh_w2.id]
+#   tags = { Name = "agent",
+#     owner = "nick.philbrook@hashicorp.com",
+#     TTL   = 0
+#   }
+#   lifecycle {
+#     ignore_changes = [ami]
+#   }
+# }
 
-resource "aws_route53_record" "agent" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "agent.w2.${local.subdomain}"
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.agent.public_ip]
-}
+# resource "aws_route53_record" "agent" {
+#   zone_id = aws_route53_zone.primary.zone_id
+#   name    = "agent.w2.${local.subdomain}"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_instance.agent.public_ip]
+# }
 
 resource "aws_instance" "agent_supervised" {
   provider                    = aws.usw2
