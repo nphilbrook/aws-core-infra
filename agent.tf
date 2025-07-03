ab697cb303ab4b4a51d1c86a5a8e491caad11536
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-07-03 07:16:29 -0400

    IMDSv2

--- agent.tf
+++ agent.tf
@@ -74,6 +74,9 @@ resource "aws_instance" "agent_supervised" {
   iam_instance_profile        = aws_iam_instance_profile.agent_profile.name
   instance_type               = local.jump_instance_type
   key_name                    = aws_key_pair.acme_w2.key_name
+  metadata_options {
+    http_tokens = "required"
+  }
   user_data                   = templatefile("${path.module}/agent_user_data.tpl", { num_agents = 2 })
   user_data_replace_on_change = true
   vpc_security_group_ids      = [aws_security_group.allow_ssh_w2.id]
