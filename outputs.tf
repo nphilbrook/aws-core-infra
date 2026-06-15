9d7fa0c351336cda77c6b41ed326c3a10bc07733
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2026-06-15 09:44:04 -0400

    Remove outputs for destroyed resources

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
-output "accepter_vpc_id" {
-  value = aws_vpc_peering_connection_accepter.accept_peer.vpc_id
-}
+# output "accepter_vpc_id" {
+#   value = aws_vpc_peering_connection_accepter.accept_peer.vpc_id
+# }
 
-output "peer_vpc_id" {
-  value = aws_vpc_peering_connection_accepter.accept_peer.peer_vpc_id
-}
+# output "peer_vpc_id" {
+#   value = aws_vpc_peering_connection_accepter.accept_peer.peer_vpc_id
+# }
 
-output "peer_owner_id" {
-  value = aws_vpc_peering_connection_accepter.accept_peer.peer_owner_id
-}
+# output "peer_owner_id" {
+#   value = aws_vpc_peering_connection_accepter.accept_peer.peer_owner_id
+# }
 
-output "accepter" {
-  value = aws_vpc_peering_connection_accepter.accept_peer.accepter
-}
+# output "accepter" {
+#   value = aws_vpc_peering_connection_accepter.accept_peer.accepter
+# }
 
-output "requester" {
-  value = aws_vpc_peering_connection_accepter.accept_peer.requester
-}
+# output "requester" {
+#   value = aws_vpc_peering_connection_accepter.accept_peer.requester
+# }
 
 # output "aws_ec2_transit_gateway_id" {
 #   value = aws_ec2_transit_gateway.w2.id
